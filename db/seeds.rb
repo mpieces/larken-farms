e69e0adf96f7201abbf16bbd0bb8d88fe0f9cfaa
Marisa Ramon <mer0522@gmail.com>
2016-01-11 17:34:20 -0600

    Larken scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create!(title: 'Peaches', description: "1 lb. of juicy Whiteflesh peaches", image_url: 'peach_icon.png', price: 15.00)
