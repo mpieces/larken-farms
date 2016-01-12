ecbc811c0582121c0af5f1b942aae65456b0f5bf
Marisa Ramon <mer0522@gmail.com>
2016-01-11 20:02:09 -0600

    add quantity to line items table, migrate existing data into new table format, provide flash notice of errors, use logger to log events, remove parameter from permitted list, add css

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,11 +28,12 @@ class LineItemsController < ApplicationController
   def create
     # @line_item = LineItem.new(line_item_params)
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    # @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        # format.html { redirect_to @line_item.cart, notice: 'Item added.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -73,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
