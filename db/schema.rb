062e3e9907fb3a91f68200aa014bdeed63ef72b5
Marisa Ramon <mer0522@gmail.com>
2016-01-11 19:02:53 -0600

    create cart controller and add set cart method as module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111231300) do
+ActiveRecord::Schema.define(version: 20160112005636) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
