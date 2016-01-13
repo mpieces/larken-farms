3ef3c0d00f20039d5ed20aa987ccf77d072db8e2
Marisa Ramon <mer0522@gmail.com>
2016-01-13 16:51:56 -0600

    configure environments to enable sending outbound emails, create mailer to send confirmation emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Larken::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    user_name: "mer0522",
+    password: "dakota22",
+    enable_starttls_auto: true
+}
+end
