6be21a9477a08257a2a6d5cf6536babf36606919
Marisa Ramon <mer0522@gmail.com>
2016-01-24 19:41:44 -0600

    reformat contact us form to match other forms

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,19 @@
 class Contact < MailForm::Base
+# Added:
+  validates :name, :email, presence: true
+  validates :email, format: {
+    with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i,
+    message: 'Must be a valid email address.'
+  }
+
+
+# From form tutorial:
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
   attribute :nickname,  :captcha  => true
+
+
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
