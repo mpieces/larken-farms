89182c7daea6affe2ce388b9be754ae92d0421c0
Vic Ramon <vic@vicramon.com>
2016-01-24 20:03:08 -0800

    Don't require image_url for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :title, length: { minimum: 5}
