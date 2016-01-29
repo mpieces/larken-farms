8ce2edef84dc10b87a306a2bd9b6c91ada82f11d
Marisa Ramon <mer0522@gmail.com>
2016-01-28 21:21:25 -0600

    Try to fix cart ajax again, Style subscribe form, deleted Contact.delete_all from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
-raise "Only run this locally" unless Rails.env.development?
+# raise "Only run this locally" unless Rails.env.development?
 
 Product.delete_all
 User.delete_all
 Cart.delete_all
 LineItem.delete_all
-Contact.delete_all
+# Contact.delete_all
 Order.delete_all
 
 # Products
-Product.create!(
+Product.create(
   title: 'Peaches',
   description: "1 lb. of juicy Whiteflesh peaches",
   image_url: 'peach-icon.png',
   price: 15.00
 )
-Product.create!(
+Product.create(
   title: 'blackberries',
   description: "I am a description",
   image_url: 'bb-icon.png',
   price: 20.00
 )
 
-Product.create!(
+Product.create(
   title: 'Cherries',
   description: "I am a cherry description",
   image_url: 'cherries.jpg',
