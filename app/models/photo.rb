8dc9801c8cb66bd7f5e15b2722dea3e089678616
Marisa Ramon <mer0522@gmail.com>
2016-04-28 21:58:12 -0500

    Add credentials, bucket name, and endpoint to Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,13 +2,13 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :image,
    styles: { thumb: ["64x64#", :jpg], original: ["400x400>", :jpg]},
-   convert_options: { thumb: "-quality 75 -strip", original: "-quality 85 -strip" }
-   storage: :s3
-   # s3_credentials: {access_key_id: , secret_access_key: },
-   # bucket: "larkenfarms",
+   convert_options: { thumb: "-quality 75 -strip", original: "-quality 85 -strip" },
+   storage: :s3,
+   s3_credentials: {access_key_id: ENV["AWS_KEY"] , secret_access_key: ENV["AWS_SECRET"] },
+   bucket: "larkenfarms",
    # :s3_permissions => :private,
    # :region => "us-west-2",
-   # :endpoint => "larkenfarms.s3.amazonaws.com"
+   :endpoint => "larkenfarms.s3.amazonaws.com"
 
   validates_attachment :image, presence: true,
    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
