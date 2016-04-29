c4b9b6e54b5bdb594656a7e6d59907911bfe5115
Marisa Ramon <mer0522@gmail.com>
2016-04-28 21:11:39 -0500

    Verify that image uploader working, change specified image size to original

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
 
   has_attached_file :image,
-   styles: { thumb: ["64x64#", :jpg], original: ["500x500>", :jpg]},
+   styles: { thumb: ["64x64#", :jpg], original: ["400x400>", :jpg]},
    convert_options: { thumb: "-quality 75 -strip", original: "-quality 85 -strip" }
    # storage: :s3,
    # s3_credentials: {access_key_id: "AKIAIQ6D7DI64E63YICA", secret_access_key: "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"},
@@ -10,7 +10,7 @@ class Photo < ActiveRecord::Base
    # :region => "us-west-2",
    # :endpoint => "larkenfarms.s3.amazonaws.com"
 
-  validates_attachment :image,
+  validates_attachment :image, presence: true,
    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
    size: { in: 0..500.kilobytes }
 
