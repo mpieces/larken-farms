319c2a279ef82a4b8df02fcc786fcc934c72486f
Marisa Ramon <mer0522@gmail.com>
2016-04-29 19:33:50 -0500

    Remove region and endpoint from photo.rb model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,9 +6,9 @@ class Photo < ActiveRecord::Base
    storage: :s3,
    s3_credentials: {access_key_id: ENV["AWS_KEY"] , secret_access_key: ENV["AWS_SECRET"] },
    bucket: "larkenfarms",
-   :s3_permissions => :private,
-   :region => "us-west-2",
-   :endpoint => "larkenfarms.s3.amazonaws.com"
+   :s3_permissions => :private
+   # :region => "us-west-2",
+   # :endpoint => "larkenfarms.s3.amazonaws.com"
 
   validates_attachment :image, presence: true,
    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
