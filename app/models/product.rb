0b89bd55af46b83c9685946b9676cf843317783c
Marisa Ramon <mer0522@gmail.com>
2016-05-02 12:11:12 -0500

    Take out price functionality for products, remove some validations for produce

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,9 @@ class Product < ActiveRecord::Base
   before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  # validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-  validates :title, length: { minimum: 5}
+  # validates :title, length: { minimum: 5}
   validates :image_url, allow_nil: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'Must be a URL for GIF, JPG or PNG image.' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
+
+
 end
+
