45b19766345730a81614b6769c745f7543a0f09b
Marisa Ramon <mer0522@gmail.com>
2016-05-02 23:25:57 -0500

    Add additional email address to send contact form to

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -18,8 +18,8 @@ class Contact < MailForm::Base
   # Declare the e-mail headers. It accepts anything the mail method in ActionMailer accepts.
   def headers
     {
-      :subject => "Contact Form",
-      :to => "mer0522@gmail.com",
+      :subject => "Contact Form Message",
+      :to => "mer0522@gmail.com", "paincenterdallas@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
