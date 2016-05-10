a10eccba733f843f9936478d0d781892e6b2428d
Marisa Ramon <mer0522@gmail.com>
2016-05-10 09:35:25 -0500

    Comment out index method in Admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,8 @@
 class AdminController < AuthenticatedController
 
   # the index action sets up the count of orders
-  def index
-    @total_orders = Order.count
-  end
+  # def index
+  #   @total_orders = Order.count
+  # end
 
 end
