0b5302938b3d1e2b8333bee50840b1854d123ab8
Marisa Ramon <mer0522@gmail.com>
2016-05-15 20:49:31 -0500

    Comment in Paperclip validator in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -32,9 +32,9 @@ class Product < ActiveRecord::Base
   end
 
 
-  # validates_attachment
-  # :image, presence: true,
-  # content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+  validates_attachment
+  :image, presence: true,
+  content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 
   # private
 
