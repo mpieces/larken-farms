08885b822cbda37332b0fccc7a755b4502d2ae94
Marisa Ramon <mer0522@gmail.com>
2016-06-03 00:26:53 -0500

    Delete all controllers and models not in use anymore

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,8 @@
 class Product < ActiveRecord::Base
 
-  # has_many :line_items
-  # has_many :orders, through: :line_items
-
-  # before_destroy :ensure_not_referenced_by_any_line_item
-
   validates :title, :description, presence: true
   # validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
-
-  # validates :image_url, allow_nil: true, format: {
-  #   with:    %r{\.(gif|jpg|png)\Z}i,
-  #   message: 'Must be a URL for GIF, JPG or PNG image.' }
 
   def self.latest
     Product.order(:updated_at).last
@@ -36,14 +27,5 @@ class Product < ActiveRecord::Base
    content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
    size: { in: 0..500.kilobytes }
 
-  # private
 
-  #   #ensure that there are no line items referencing this product
-  #   def ensure_not_referenced_by_any_line_item
-  #     if line_items.empty?
-  #       return true
-  #     else
-  #       errors.add(:base, 'Line Items present.')
-  #     end
-  #   end
 end
